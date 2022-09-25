27499d1e518dc86a2bb8808cb34a75cb00f97792
chrissieaerospace <chrissie@cartoonmango.com>
2022-09-25 22:32:37 +0530

    [CLEANED-UP] - CODE

--- Shared/Routes/index.js
+++
@@ -1,4 +0,0 @@
-export { default as RoutesContainer } from './routes';
-export { default as UiRouteConfig } from './ui-route-config';
-export { default as FeRouteConfig } from './fe-route-config';
-        
